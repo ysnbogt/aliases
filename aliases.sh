6d325160f66399e94f2732118c268e6d0dcdea2c
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2023-04-27 12:50:17 +0900

    update

--- aliases.sh
+++ aliases.sh
@@ -8,51 +8,101 @@ lorem_ipsum='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu
  voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-const c            = 'code .'
-const o            = 'open .'
-const p            = 'pbcopy'
+const c           = 'code .'
+const o           = 'open .'
+const p           = 'pbcopy'
 
-const todayd       = 'mkdir $(date "+%Y-%m-%d")'
-const todayf       = 'touch $(date "+%Y-%m-%d").md'
-const readme       = 'touch README.md'
-const makefile     = 'touch Makefile'
-const vs           = 'mkdir .vscode && touch .vscode/settings.json'
-const workflow     = 'touch .github/workflows/main.yml'
+const cdd         = 'cd ~/Desktop'
+const cdtoday     = 'cd $(date "+%Y-%m-%d")'
 
-const stop-all     = 'docker stop $(docker ps -q)'
-const rm-all       = 'docker rm $(docker ps -a -q)'
-const rmi-all      = 'docker rmi $(docker images -q)'
+const todayd      = 'mkdir $(date "+%Y-%m-%d")'
+const vsc         = 'mkdir .vscode && touch .vscode/settings.json'
 
-const pangram      = 'echo "The quick brown fox jumps over the lazy dog" | pbcopy'
-const lorem-ipsum  = "echo '$lorem_ipsum' | pbcopy"
+const readme      = 'touch README.md'
+const makefile    = 'touch Makefile'
+const workflow    = 'touch .github/workflows/main.yml'
 
-const 1m           = 'countdown 60 3'
-const 2m           = 'countdown 120 3'
-const 3m           = 'countdown 180 3'
-const 5m           = 'countdown 300 3'
-const 10m          = 'countdown 600 3'
+const stop-all    = 'docker stop $(docker ps -q)'
+const rm-all      = 'docker rm $(docker ps -a -q)'
+const rmi-all     = 'docker rmi $(docker images -q)'
 
-const 8080         = 'open http://localhost:8080'
-const 3000         = 'open http://localhost:3000'
-const 5173         = 'open http://localhost:5173'
+const pangram     = 'echo "The quick brown fox jumps over the lazy dog" | pbcopy'
+const lorem-ipsum = "echo '$lorem_ipsum' | pbcopy"
+
+const 1m          = 'countdown 60 3'
+const 2m          = 'countdown 120 3'
+const 3m          = 'countdown 180 3'
+const 5m          = 'countdown 300 3'
+const 10m         = 'countdown 600 3'
+
+const 8080        = 'open http://localhost:8080'
+const 3000        = 'open http://localhost:3000'
+const 5173        = 'open http://localhost:5173'
 
 jdiff() {
+    # Comparing Two JSON Files
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ jdiff <first-file> <second-file>                                                        │
+    # │ $ jdiff a.json b.json                                                                     │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     diff <(jq --sort-keys . $1) <(jq --sort-keys . $2)
 }
 
 ppng() {
+    # Copying an image to the clipboard
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ ppng <image-path>                                                                       │
+    # │ $ ppng image.png                                                                          │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     osascript -e 'set the clipboard to (read (POSIX file "'$1'") as JPEG picture)'
 }
 
+ccpng() {
+    # Make the code into an image and then copy it to the clipboard.
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ brew install silicon                                                                    │
+    # │ $ ccpng main.py                                                                           │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
+    silicon $1 -o $1.tmp.png \
+    && osascript -e 'set the clipboard to (read (POSIX file "'$1.tmp.png'") as JPEG picture)' \
+    && rm $1.tmp.png
+}
+
 snake() {
+    # Snake case a string and copy it to the clipboard
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ snake "Hello World"  # hello_world                                                      │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     python3 -c 'import sys; print("_".join(sys.argv[1].lower().split(" ")), end="")' $1 | pbcopy
 }
 
 camel() {
+    # CamelCase a string and copy it to the clipboard
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ snake "Hello World"  # HelloWorld                                                       │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     python3 -c 'import sys; print("".join(sys.argv[1].title().split(" ")), end="")' $1 | pbcopy
 }
 
 countdown() {
+    # Countdown for specified time. `Ctrl+C` to stop.
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ countdown <time> [number-of-sounds]                                                     │
+    # │ $ countdown 60   # One minute later, no sound and exit.                                   │
+    # │ $ countdown 5 1  # After 5 seconds, one sound and exit.                                   │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     countdown_seconds=$1
     start_time=$(date +%s)
     end_time=$((start_time + countdown_seconds))
@@ -76,12 +126,27 @@ countdown() {
     # Play as many notes as the number of the second argument.
     if [ $# -eq 2 ]; then
         for i in $(seq 1 $2); do
+            # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────╮
+            # ├───────────────────────────────────────────────────────────────────────────────────┤
+            # │ $ ls /System/Library/Sounds                                                       │
+            # │ Basso.aiff     Frog.aiff      Hero.aiff      Pop.aiff       Submarine.aiff        │
+            # │ Blow.aiff      Funk.aiff      Morse.aiff     Purr.aiff      Tink.aiff             │
+            # │ Bottle.aiff    Glass.aiff     Ping.aiff      Sosumi.aiff                          │
+            # ╰───────────────────────────────────────────────────────────────────────────────────╯
+
+            # Of course, original audio files can be specified.
             afplay /System/Library/Sounds/Funk.aiff
         done
     fi
 }
 
 stopwatch() {
+    # Stopwatch. `Ctrl+C` to stop.
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ stopwatch                                                                               │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
     start_time=$(date +%s)
     seconds=0
     minutes=0
@@ -96,4 +161,38 @@ stopwatch() {
         printf "\r%02d:%02d:%02d" $hours $minutes $seconds
         sleep 1
     done
+}
+
+todayf() {
+    # Create a file with today's date.
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ todayf <file-extension>                                                                 │
+    # │ $ todayf md                                                                               │
+    # │ $ todayf py                                                                               │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
+    touch $(date "+%Y-%m-%d").$1
+}
+
+color() {
+    # Display colors in the console. Text color is the opposite of the specified color
+
+    # ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────────────╮
+    # ├───────────────────────────────────────────────────────────────────────────────────────────┤
+    # │ $ color <hex-code>                                                                        │
+    # │ $ color 008000  # Green                                                                   │
+    # │ $ color FF0000  # Red                                                                     │
+    # │ $ color 0000FF  # Blue                                                                    │
+    # ╰───────────────────────────────────────────────────────────────────────────────────────────╯
+    perl -e '
+    foreach $a(@ARGV) {
+        my($r, $g, $b) = unpack("C*", pack("H*", $a));
+        my $inv_r = 255 - $r;
+        my $inv_g = 255 - $g;
+        my $inv_b = 255 - $b;
+        printf "\e[48:2:%d:%d:%dm\e[38:2:%d:%d:%dm %s \e[0m", $r, $g, $b, $inv_r, $inv_g, $inv_b, $a;
+    }
+    print "\n";
+    ' $1
 }
